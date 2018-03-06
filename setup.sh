df113f8e91f83ceb8ab3e4744cb872f4a4aae0f8
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-03-06 18:54:01 +0000

    setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,3 @@ git add README.md
 git commit -m "first commit"
 git remote add origin https://github.com/jinghuazhao/physalia.git
 git push -u origin master
-
-git remote add origin https://github.com/jinghuazhao/physalia.git
-git push -u origin master
