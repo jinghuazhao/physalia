a428eda81ffec190c9b63cdc3e334b22f3089eea
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-05-02 14:14:40 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,10 +1,10 @@
-# 6-3-2018 MRC-Epid JHZ
+# 2-5-2018 MRC-Epid JHZ
 
 git add README.md
 git commit -m "README"
 git add setup.sh
 git commit -m "setup.sh"
-git add 2367.rar
+git add 2367.zip BLR.zip
 git commit -m "JSS paper"
 git add st.sh
 git commit -m "st.sh"
