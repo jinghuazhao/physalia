111263917b99e6a093695809103be81d4cb152cd
jinghuazhao <jinghuazhao@hotmail.com>
2018-05-19 14:54:24 +0100

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
+# 19-5-2018 JHZ
+
+# git config remote.origin.url https://jinghuazhao:{PASSWORD}@github.com/jinghuazhao/physalia.git
+# This looks to avoid use of ssh
+# check ~/.gitconfig
+
 git config --global user.email "jinghuazhao@hotmail.com"
 git config --global user.name "jinghuazhao@github.com"
-
-git config remote.origin.url https://jinghuazhao:{PASSWORD}@github.com/jinghuazhao/physalia.git
 git config --global url."https://jinghuazhao@github.com".insteadOf "https://github.com"
 git config --global credential.helper 'cache --timeout=86400'
 
-# check ~/.gitconfig
-
 git init
 git add README.md
-git commit -m "first commit"
+git commit -m "README.md"
 git remote add origin https://github.com/jinghuazhao/physalia.git
 git push -u origin master
 
