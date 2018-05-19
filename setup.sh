067d289f7755928bcf3442691d6ea666e40fc198
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-05-19 10:58:16 +0100

    setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,14 @@ git add README.md
 git commit -m "first commit"
 git remote add origin https://github.com/jinghuazhao/physalia.git
 git push -u origin master
+
+git config remote.origin.url https://jinghuazhao:{PASSWORD}@github.com/jinghuazhao/physalia.git
+git config --global url."https://jinghuazhao@github.com".insteadOf "https://github.com"
+git config --global credential.helper 'cache --timeout=86400'
+
+git config --global user.email "jinghuazhao@hotmail.com"
+git config --global user.name "jinghuazhao@github.com"
+
+# check ~/.gitconfig
+
+#
