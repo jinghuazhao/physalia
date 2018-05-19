fa55bd564798a0104344b0b5acd034ce5af6fb90
jinghuazhao <jinghuazhao@hotmail.com>
2018-05-19 20:02:29 +0100

    ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,21 +1,14 @@
 # 19-5-2018 JHZ
 
-# Adapted from the following URL,
-# https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account/
-
-ls -al ~/.ssh
 ssh-keygen -t rsa -b 4096 -C "jinghua@hotmail.com"
 # paraphrase, name
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
+ls -al ~/.ssh
 
 sudo apt install xsel
 xsel --clipboard < ~/.ssh/id_rsa.pub
-
-# clip is actually a Windows application
-# clip < ~/.ssh/id_rsa.pub
-# Ubuntu 18.04 gives the (wrong but otherwise useful) suggestion:
-# sudo apt install geomview
+# paste into GitHub settings for SSH
 
 ssh -T git@github.com
 
@@ -23,3 +16,10 @@ ssh-add -l -E md5
 
 # later change in paraphrase
 # ssh-keygen -p
+
+# For additional information, see
+# https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account/
+# clip described at GitHub is actually a Windows application
+# clip < ~/.ssh/id_rsa.pub
+# Ubuntu 18.04 gives the (wrong but otherwise useful) suggestion:
+# sudo apt install geomview
