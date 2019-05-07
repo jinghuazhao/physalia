de54a252f36db0a18370c22955908339cc4fe68b
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-05-07 01:02:20 +0100

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-# 8-9-2018 JHZ
+# 7-5-2019 JHZ
 
 # git config remote.origin.url https://jinghuazhao:{PASSWORD}@github.com/jinghuazhao/physalia.git
 # This simplifies matters since only one login is required per session to push commits.
@@ -9,8 +9,16 @@ git config --global user.name "jinghuazhao@github.com"
 git config --global url."https://jinghuazhao@github.com".insteadOf "https://github.com"
 git config --global credential.helper 'cache --timeout=86400'
 
+# Suggestions from web setup
+# create a new repository on the command line
 git init
 git add README.md
 git commit -m "README.md"
 git remote add origin https://github.com/jinghuazhao/physalia.git
 git push -u origin master
+
+# push an existing repository from the command line
+git remote add origin https://github.com/jinghuazhao/GWAS-2017.git
+git push -u origin master
+
+# import code from another repository
