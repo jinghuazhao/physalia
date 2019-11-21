6b28a438063c5352ee9692d1746844ef83d3c54f
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-11-21 08:44:18 +0000

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,18 @@
-# 24-5-2019 JHZ
+# 21-11-2019 JHZ
+
+# create a repository on the command line
+
+git init physalia
+cd physalia
+echo README > README.md
+git add README.md
+git commit -m "README.md"
+
+# push from the command line
+git remote add origin https://github.com/jinghuazhao/physalia.git
+git push -u origin master
+
+# import code from another repository
 
 # git config remote.origin.url https://jinghuazhao:{PASSWORD}@github.com/jinghuazhao/physalia.git
 # This simplifies matters since only one login is required per session to push commits.
@@ -8,34 +22,3 @@ git config --global user.email "jinghuazhao@hotmail.com"
 git config --global user.name "jinghuazhao@github.com"
 git config --global url."https://jinghuazhao@github.com".insteadOf "https://github.com"
 git config --global credential.helper 'cache --timeout=86400'
-
-# Suggestions from web setup
-# create a new repository on the command line
-git init physalia
-cd physalia
-echo README > README.md
-git add README.md
-git commit -m "README.md"
-git remote add origin https://github.com/jinghuazhao/physalia.git
-git push -u origin master
-
-# push an existing repository from the command line
-git remote add origin https://github.com/jinghuazhao/GWAS-2017.git
-git push -u origin master
-
-# import code from another repository
-
-# The option above is with respect to https, and for ssh it would be 
-
-git init GitHub-matters
-cd GitHub-matters
-echo "# GitHub-matters" >> README.md
-git add README.md
-git commit -m "first commit"
-git remote add origin git@github.com:cambridge-ceu/GitHub-matters.git
-git push -u origin master
-
-# …or push an existing repository from the command line
-
-git remote add origin git@github.com:cambridge-ceu/GitHub-matters.git
-git push -u origin master
