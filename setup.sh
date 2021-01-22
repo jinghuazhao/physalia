ec44c3d575922f0e752c28952e667c4aa05b7518
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-01-22 22:03:26 +0000

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-# 21-11-2019 JHZ
+# 22-1-2021 JHZ
 
 # git config remote.origin.url https://jinghuazhao:{PASSWORD}@github.com/jinghuazhao/physalia.git
 # This simplifies matters since only one login is required per session to push commits.
@@ -7,8 +7,15 @@
 git config --global user.email "jinghuazhao@hotmail.com"
 git config --global user.name "jinghuazhao@github.com"
 git config --global url."https://jinghuazhao@github.com".insteadOf "https://github.com"
+# cache password 24hr long
+# Linux = cache, Windows = wincred, Mac = osxkeychain (two ways below)
+# git credential.helper-osxkeychain
+# brew install git
 git config --global credential.helper 'cache --timeout=86400'
 
+# switch remote URLs from https to ssh
+git remote -v
+git remote set-url origin git@github.com:jinghuazhao/physalia.git
 # create a repository on the command line
 
 git init physalia
