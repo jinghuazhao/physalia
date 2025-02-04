1aac8598cb9eb1e78400856b952a4721c1cdd5d0
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-02-04 11:16:47 +0000

    Deployed 739636d with MkDocs version: 1.5.3

--- slides/slides.sh
+++ slides/slides.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+# ImageMagick
+wget https://www.cam.ac.uk/sites/all/themes/fresh/images/interface/cambridge_university2.svg
+convert -density 300 cambridge_university2.svg -resize 30% -units pixelsperinch ucam.png
+
 # pandoc
 module load texlive
 pandoc -t beamer --citeproc --mathjax -s -V theme:metropolis slides.md -o slides.pdf
