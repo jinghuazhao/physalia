187a8cbbf47e44f592a62384adba7f429f3ec429
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-02-22 22:08:26 +0000

    Deployed a3ce486 with MkDocs version: 1.5.3

--- slides/slides.sh
+++ slides/slides.sh
@@ -4,6 +4,12 @@
 wget https://www.cam.ac.uk/sites/all/themes/fresh/images/interface/cambridge_university2.svg
 convert -density 300 cambridge_university2.svg -resize 30% -units pixelsperinch ucam.png
 
+# video
+# -ss 00:00:01: Snapshot at 1 second into the video
+# -vframes 1: Save 1 frame as poster-image.jpg
+module load ceuadmin/ffmpeg
+ffmpeg -i plum-popcorn.mp4 -ss 00:00:01 -vframes 1 poster-image.jpg
+
 # pandoc
 module load texlive
 pandoc -t beamer --citeproc --mathjax -s -V theme:metropolis slides.md -o slides.pdf
@@ -12,9 +18,3 @@ pandoc -t revealjs --citeproc --mathjax -s -V theme:metropolis -s slides.md -o s
 # marp
 module load ceuadmin/node
 marp slides.md
-
-# video
-# -ss 00:00:01: Snapshot at 1 second into the video
-# -vframes 1: Save 1 frame as poster-image.jpg
-module load ceuadmin/ffmpeg
-ffmpeg -i plum-popcorn.mp4 -ss 00:00:01 -vframes 1 poster-image.jpg
